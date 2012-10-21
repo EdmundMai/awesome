f07ffac39e7353a925adccea317998f88e41bfb4
Edmund Mai <edmundmai@gmail.com>
2012-10-21 15:36:58 -0400

    made ajax

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121021180621) do
+ActiveRecord::Schema.define(:version => 20121021182021) do
+
+  create_table "aliens", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "pirates", :force => true do |t|
     t.string   "name"
