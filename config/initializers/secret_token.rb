754ae8da6340249e716b9b4afbae9144ddabba12
Edmund Mai <edmundmai@gmail.com>
2012-10-21 13:40:19 -0400

    new awesome app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awesome::Application.config.secret_token = 'ae765435ea35d030437e5d563d0c4d137ba52f7c720a3b025928a6c42c89384ac402135cab0bc04e41bfb159d7ed08d8e1f53939ce64e3dab1b7ad7c4a5e3200'
